59f60041a358b62790a0f271f581a44593d20dd2
Felipe Lopes <bolzin@gmail.com>
2017-11-25 12:40:55 -0200

    :wrench: add credit card form

--- app/code/community/Mundipagg/Paymentmodule/Block/Form/Creditcard.php
+++ app/code/community/Mundipagg/Paymentmodule/Block/Form/Creditcard.php
@@ -8,4 +8,10 @@ class Mundipagg_Paymentmodule_Block_Form_Creditcard extends Mage_Payment_Block_F
         parent::_construct();
         $this->setTemplate('paymentmodule/form/creditcard.phtml');
     }
+
+    public function getPublicKey()
+    {
+        $generalConfig = Mage::getModel('paymentmodule/config_general');
+        return $generalConfig->getPublicKey();
+    }
 }
