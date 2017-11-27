3ecabd2e2e3743b4cd44773e173a9f8fcb798594
felipebool <bolzin@gmail.com>
2017-11-27 18:10:32 -0200

    :heavy_plus_sign: add js

--- js/mundipagg/paymentmodule.js
+++ js/mundipagg/paymentmodule.js
@@ -1,2 +1,29 @@
-console.log('hey!');
-alert('hello');
+var callbacks = {
+    success: function (data) {
+        console.log('sucess');
+        console.log(data);
+        return false;
+    },
+
+    failure: function (data) {
+        console.log('failure');
+        console.log(data);
+        return false;
+    }
+};
+
+Payment.prototype.save = Payment.prototype.save.wrap(function(save) {
+    // I'm not proud of this... :(
+    var key = document.getElementById('tokenDiv').getAttribute('data-pkkey');
+    document.getElementById('placeLixo').setAttribute('data-mundicheckout-app-id',key);
+
+    // start checkout
+    MundiCheckout.init(callbacks.success, callbacks.failure);
+
+    
+    var elem = document.getElementById('tokenDiv');
+    var event = new Event('submit');
+    elem.dispatchEvent(event);
+
+    save();
+});
