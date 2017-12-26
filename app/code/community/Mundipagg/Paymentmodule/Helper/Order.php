f7a59b9dae1cb73b01cae17029e0a719a5be58d9
Ian Lessa <ian.lessa1@gmail.com>
2017-12-26 15:50:49 -0200

    :sparkles: Order helper added.

---
+++ app/code/community/Mundipagg/Paymentmodule/Helper/Order.php
@@ -0,0 +1,45 @@
+<?php
+
+use MundiAPILib\Models\GetOrderResponse;
+
+class Mundipagg_Paymentmodule_Helper_Order extends Mage_Core_Helper_Abstract
+{
+    /**
+     * Change the status of an order, fetched by the code field in order object, on $orderResponse param.
+     * If $statusCode is null, the new status of the order will be defined by the status field on the order object.
+     *
+     * @param GetOrderResponse $orderResponse
+     * @param string|null $statusCode If null, get the status from order object.
+     * @throws Exception
+     */
+    public function updateStatus(GetOrderResponse $orderResponse, $statusCode = null)
+    {
+        $status = $statusCode;
+        if (!$status) {
+            $statusMap = $this->getStatusMap();
+            $status = $statusMap[$orderResponse->status];
+        }
+        $newHistory = new Mage_Sales_Model_Order_Status_History();
+        $newHistory->setStatus($status);
+
+        $order = new Mage_Sales_Model_Order();
+        $order->loadByIncrementId($orderResponse->code);
+        $order->addStatusHistory($newHistory);
+        $order->save();
+    }
+
+    /**
+     * Returns an array map for order object statuses.
+     *
+     * @return array
+     */
+    public function getStatusMap()
+    {
+        return [
+            'Pending'   => 'pending',
+            'Paid'      => Mage_Sales_Model_Order::STATE_PROCESSING,
+            'Canceled'  => Mage_Sales_Model_Order::STATE_CANCELED,
+            'Failed'    => Mage_Sales_Model_Order::STATE_CANCELED,
+        ];
+    }
+}
