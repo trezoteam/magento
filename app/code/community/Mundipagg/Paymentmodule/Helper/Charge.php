c8ccd8e467175f61899829a56b391c5b58538614
Ian Lessa <ian.lessa1@gmail.com>
2017-12-27 16:02:20 -0200

    :memo: Add PHPDoc for method.

--- app/code/community/Mundipagg/Paymentmodule/Helper/Charge.php
+++ app/code/community/Mundipagg/Paymentmodule/Helper/Charge.php
@@ -4,6 +4,12 @@ use MundiAPILib\Models\GetChargeResponse;
 
 class Mundipagg_Paymentmodule_Helper_Charge extends Mage_Core_Helper_Abstract
 {
+    /**
+     * Add the status of a charge to the history of the order.
+     *
+     * @param GetChargeResponse $chargeResponse
+     * @throws Exception
+     */
     public function updateStatus(GetChargeResponse $chargeResponse)
     {
         $order = new Mage_Sales_Model_Order();
