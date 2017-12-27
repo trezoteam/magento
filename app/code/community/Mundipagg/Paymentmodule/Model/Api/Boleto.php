69c6b8ca8100b84014db6e22ee29d9217a9bba0d
Ian Lessa <ian.lessa1@gmail.com>
2017-12-27 14:29:56 -0200

    :bug: Fixed items field of Boleto Request object

--- app/code/community/Mundipagg/Paymentmodule/Model/Api/Boleto.php
+++ app/code/community/Mundipagg/Paymentmodule/Model/Api/Boleto.php
@@ -21,30 +21,13 @@ class Mundipagg_Paymentmodule_Model_Api_Boleto
         $checkoutSession = $standard->getCheckoutSession();
         $orderId = $checkoutSession->getLastRealOrderId();
 
-        $orderRequest->items = $this->getItems($paymentInformation->getItemsInfo());
+        $orderRequest->items = $paymentInformation->getItemsInfo();
         $orderRequest->customer = $this->getCustomerRequest($paymentInformation->getCustomerInfo());
         $orderRequest->payments = $this->getPayments($paymentInformation->getPaymentInfo());
         $orderRequest->code = $orderId;
         $orderRequest->metadata = $paymentInformation->getMetainfo();
 
         return $orderRequest;
-    }
-
-    private function getItems($itemsInfo)
-    {
-        $items = [];
-
-        foreach ($itemsInfo as $item) {
-            $orderItemRequest = new CreateOrderItemRequest();
-
-            $orderItemRequest->amount = $item->getAmount();
-            $orderItemRequest->quantity = $item->getQuantity();
-            $orderItemRequest->description = $item->getDescription();
-
-            $items[] = $orderItemRequest;
-        }
-
-        return $items;
     }
 
     private function getCustomerRequest($customerInfo)
