5d6405773072340ac5735522551b064f68498450
Michel <michelpl@yahoo.com.br>
2018-01-23 20:48:09 -0200

    :bug: Add missing enum.

--- app/code/community/Mundipagg/Paymentmodule/Model/Enum/Orderhistory.php
+++ app/code/community/Mundipagg/Paymentmodule/Model/Enum/Orderhistory.php
@@ -5,6 +5,7 @@ class Mundipagg_Paymentmodule_Model_Enum_Orderhistory
     private $chargeCreated  = "MP - Charge created";
     private $chargePaid  = "MP - Charge paid - ";
     private $chargeOverpaid = "MP - Charge orverpaid - ";
+    private $chargeUnderpaid = "MP - Charge underpaid - ";
 
     public function __call($name, $arguments)
     {
