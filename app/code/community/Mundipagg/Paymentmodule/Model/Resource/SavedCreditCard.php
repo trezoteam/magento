9b8c377c3c97d98cc15c306be549ec8d32ab1f64
Michel <michelpl@yahoo.com.br>
2018-03-14 17:06:26 -0300

    :sparkles: Create saved credit card table.

---
+++ app/code/community/Mundipagg/Paymentmodule/Model/Resource/SavedCreditCard.php
@@ -0,0 +1,9 @@
+<?php
+
+class Mundipagg_Paymentmodule_Model_Resource_SavedCreditCard extends Mage_Core_Model_Resource_Db_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('mundipagg_paymentmodule/saved_credit_card', 'card_id');
+    }
+}
---
+++ app/code/community/Mundipagg/Paymentmodule/Model/SavedCreditCard.php
@@ -0,0 +1,9 @@
+<?php
+
+class Mundipagg_Paymentmodule_Model_SavedCreditCard extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('mundipagg_paymentmodule/saved_credit_card');
+    }
+}
