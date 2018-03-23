6f7cf3ab9980bcdc934ea7b7bc6252bafe95f928
Ian Lessa <ian.lessa1@gmail.com>
2018-03-23 11:56:55 -0300

    :construction: add creditcard wallet link on account menu

--- app/code/community/Mundipagg/Paymentmodule/Model/Observer.php
+++ app/code/community/Mundipagg/Paymentmodule/Model/Observer.php
@@ -1,6 +1,12 @@
 <?php
 
 
-class Mundipagg_Paymentmodule_Model_Observer
+class Mundipagg_Paymentmodule_Model_Observer extends Varien_Event_Observer
 {
+
+    public function addAccountCreditcardWalletMenuItem( Varien_Event_Observer $observer)
+    {
+        $update = $observer->getEvent()->getLayout()->getUpdate();
+        $update->addHandle('creditcard_wallet_menu_item_handle');
+    }
 }
