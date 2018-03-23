4dbd47c6bec3ca3ebf4948f67e669ce0e9a89aac
Ian Lessa <ian.lessa1@gmail.com>
2018-03-23 10:51:43 -0300

    :construction: add event observer

--- app/code/community/Mundipagg/Paymentmodule/Model/Observer.php
+++ app/code/community/Mundipagg/Paymentmodule/Model/Observer.php
@@ -1,6 +1,14 @@
 <?php
 
 
-class Mundipagg_Paymentmodule_Model_Observer
+class Mundipagg_Paymentmodule_Model_Observer extends Varien_Event_Observer
 {
+
+    public function addAdditionalInformationToCheckout() {
+        $block = Mage::app()->getLayout()->createBlock(
+            'Mundipagg_Paymentmodule_Block_Checkout_Information'
+        );
+        Mage::app()->getLayout()->getBlock('checkout.success')->append($block);
+    }
+
 }
