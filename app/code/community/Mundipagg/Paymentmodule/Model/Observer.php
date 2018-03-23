f6ea0485db3944df45361143302777c46855f1a0
Ian Lessa <ian.lessa1@gmail.com>
2018-03-23 11:06:35 -0300

    :construction:add observer trigger

--- app/code/community/Mundipagg/Paymentmodule/Model/Observer.php
+++ app/code/community/Mundipagg/Paymentmodule/Model/Observer.php
@@ -4,7 +4,8 @@
 class Mundipagg_Paymentmodule_Model_Observer extends Varien_Event_Observer
 {
 
-    public function addAdditionalInformationToCheckout() {
+    public function addAdditionalInformationToCheckout()
+    {
         $block = Mage::app()->getLayout()->createBlock(
             'Mundipagg_Paymentmodule_Block_Checkout_Information'
         );
