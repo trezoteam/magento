27b7ef1144caee515cff00710e65c05658466c66
Michel Lima <michelpl@yahoo.com.br>
2018-04-06 18:48:53 -0300

    :bug: Allow only saved credit cards with enabled brands.

--- app/code/community/Mundipagg/Paymentmodule/Block/Form/Partial/Creditcard.php
+++ app/code/community/Mundipagg/Paymentmodule/Block/Form/Partial/Creditcard.php
@@ -14,24 +14,68 @@ class Mundipagg_Paymentmodule_Block_Form_Partial_Creditcard extends Mundipagg_Pa
         return $generalConfig->getPublicKey();
     }
 
-    public function getSavedCreditCards()
+    /**
+     * Return saved cards for logged in customers
+     * @param boolean $onlyEnabledCards Only enabled card brands in admin panel
+     * @return array
+     */
+    public function getSavedCreditCards($onlyEnabledCards = true)
     {
         $session = Mage::getSingleton('customer/session');
-        $savedCreditCardsHelper = Mage::helper('paymentmodule/savedcreditcard');
+        $savedCreditCardsHelper =
+            Mage::helper('paymentmodule/savedcreditcard');
 
         if (
             $session->isLoggedIn() &&
             $savedCreditCardsHelper->isSavedCreditCardsEnabled()
         ) {
+            if ($onlyEnabledCards) {
+                return $this->enabledSavedCreditCards(
+                    $savedCreditCardsHelper->getCurrentCustomerSavedCards()
+                );
+            }
+
             return $savedCreditCardsHelper->getCurrentCustomerSavedCards();
         }
 
         return [];
     }
 
+    private function enabledSavedCreditCards($savedCreditCards)
+    {
+        $enabledBrands = $this->getEnabledBrands();
+        $enabledSavedCreditCards = [];
+
+        foreach ($savedCreditCards as $savedCreditCard) {
+            $brandName = strtolower($savedCreditCard->getBrandName());
+
+            if (in_array($brandName, $enabledBrands)) {
+                $enabledSavedCreditCards[] = $savedCreditCard;
+            }
+        }
+
+        return $enabledSavedCreditCards;
+    }
+
+
     public function isSavedCreditCardsEnabled()
     {
         return Mage::helper('paymentmodule/savedcreditcard')
             ->isSavedCreditCardsEnabled();
+    }
+
+    public function getEnabledBrands() {
+        $brandStatuses = Mage::getModel('paymentmodule/config_card')
+            ->getBrandStatuses();
+
+        $enabledBrands = [];
+
+        foreach ($brandStatuses as $brand => $status) {
+            if ($status == 1) {
+                $enabledBrands[] = $brand;
+            }
+        }
+
+        return $enabledBrands;
     }
 }
