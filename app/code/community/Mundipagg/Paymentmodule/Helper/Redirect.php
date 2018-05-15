0b8a5c9bc551bfc8430b14b474eebe276141f591
Michel Lima <michelpl@yahoo.com.br>
2018-05-15 12:15:08 -0300

    :art: Create a handler for redirect actions.

---
+++ app/code/community/Mundipagg/Paymentmodule/Helper/Redirect.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class Mundipagg_Paymentmodule_Helper_Interest
+ */
+class Mundipagg_Paymentmodule_Helper_Redirect extends Mage_Core_Helper_Abstract
+{
+
+    public function orderSuccess()
+    {
+        $this->redirect('checkout/onepage/success');
+    }
+
+    public function orderFailure()
+    {
+        $this->redirect('checkout/onepage/failure');
+    }
+
+    private function redirect($route)
+    {
+        Mage::app()->getFrontController()
+            ->getResponse()
+            ->setRedirect(Mage::getUrl($route));
+    }
+}
