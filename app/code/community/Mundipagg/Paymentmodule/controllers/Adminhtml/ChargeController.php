bfdb49492d061f3546b02c60c96b0d051646a4ad
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2018-05-16 15:46:39 -0300

    :sparkles: create a new screen of charges

---
+++ app/code/community/Mundipagg/Paymentmodule/controllers/Adminhtml/ChargeController.php
@@ -0,0 +1,21 @@
+<?php
+
+class Mundipagg_Paymentmodule_Adminhtml_ChargeController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->_title($this->__('Sales'))->_title($this->__('Orders Inchoo'));
+        $this->loadLayout();
+        $this->_setActiveMenu('sales/sales');
+        $this->_addContent($this->getLayout()->createBlock('paymentmodule/adminhtml_order_charge'));
+        $this->renderLayout();
+    }
+ 
+    public function gridAction()
+    {
+        $this->loadLayout();
+        $this->getResponse()->setBody(
+            $this->getLayout()->createBlock('paymentmodule/adminhtml_order_charge_grid')->toHtml()
+        );
+    }
+ }
