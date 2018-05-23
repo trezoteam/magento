b112141f6602b933d602a8d697f956eaadbe40d2
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2018-05-23 11:50:05 -0300

    :ok_hand: Code Review

--- app/code/community/Mundipagg/Paymentmodule/controllers/MultibuyerController.php
+++ app/code/community/Mundipagg/Paymentmodule/controllers/MultibuyerController.php
@@ -2,7 +2,10 @@
 
 class Mundipagg_Paymentmodule_MultibuyerController extends Mage_Core_Controller_Front_Action
 {
-
+    /**
+     * Return json with a collection of regions
+     * @return json Region Collection
+     */
     public function getRegionsAction()
     {
         $countryCode = $this->getRequest()->getParam('country_id');
