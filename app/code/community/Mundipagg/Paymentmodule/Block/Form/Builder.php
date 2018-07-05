b1fa1185d8acee3b008775515f5435e8b682c8f8
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2018-07-05 16:35:02 -0300

    :bug: Fix Undefined index

--- app/code/community/Mundipagg/Paymentmodule/Block/Form/Builder.php
+++ app/code/community/Mundipagg/Paymentmodule/Block/Form/Builder.php
@@ -40,21 +40,25 @@ class Mundipagg_Paymentmodule_Block_Form_Builder extends Mundipagg_Paymentmodule
     public function getPartialHTML($element,$parentElement = '')
     {
         $grandTotal = $this->getGrandTotal();
-
         $retn = $this->getLayout();
+        $data = [
+            'code' => $this->getMethodCode(),
+            'element_index' => $this->getIndexFor($element),
+            'show_value_input' => count($this->getStructure()) > 1,
+            'grand_total' => number_format($grandTotal, "2", ",", ""),
+            'parent_element' => $parentElement
+        ];
+        if ($parentElement !== '') {
+            $data ['parent_index'] = $this->getElementCount()[$parentElement];
+        }
+
         $retn = $retn->createBlock("paymentmodule/form_partial_$element",'',
-            [
-                'code' => $this->getMethodCode(),
-                'element_index' => $this->getIndexFor($element),
-                'show_value_input' => count($this->getStructure()) > 1,
-                'grand_total' => number_format($grandTotal, "2", ",", ""),
-                'parent_element' => $parentElement,
-                'parent_index' => $this->getElementCount()[$parentElement]
-            ]
+            $data
         );
         $retn = $retn->toHtml();
 
         return $retn;
+
     }
 
     public function getMultiBuyerHtml($parentElement)
