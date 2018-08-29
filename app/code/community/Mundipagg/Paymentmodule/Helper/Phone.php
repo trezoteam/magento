49618b85255ddfb5d6dedbcad2d99e3cd21a80b6
Ian Lessa <ian.lessa1@gmail.com>
2018-08-29 14:35:19 -0300

    :recycle: psr

--- app/code/community/Mundipagg/Paymentmodule/Helper/Phone.php
+++ app/code/community/Mundipagg/Paymentmodule/Helper/Phone.php
@@ -5,13 +5,13 @@ class Mundipagg_Paymentmodule_Helper_Phone extends Mage_Core_Helper_Abstract
     public function extractPhoneVarienFromRawPhoneNumber($rawPhoneNumber)
     {
         $cleanPhone = preg_replace( '/[^0-9]/', '', $rawPhoneNumber);
-        $cleanPhone = ltrim($cleanPhone,'0');
+        $cleanPhone = ltrim($cleanPhone, '0');
 
         $phone = new Varien_Object();
 
         $phone->setCountryCode('55');
-        $phone->setAreacode(substr($cleanPhone,0,2));
-        $phone->setNumber(substr($cleanPhone,2));
+        $phone->setAreacode(substr($cleanPhone, 0, 2));
+        $phone->setNumber(substr($cleanPhone, 2));
 
         return $phone;
     }
