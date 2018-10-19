092cf142a518005ad5e211c7d4c1ea8ada2c976d
Michel Lima <michelpl@yahoo.com.br>
2018-10-19 18:47:28 -0300

    :construction: Createrecurrence template table creation script.

---
+++ app/code/community/Mundipagg/Paymentmodule/sql/mundipagg_paymentmodule_setup/upgrade-1.8.0-1.9.0.php
@@ -0,0 +1,29 @@
+<?php
+$installer = $this;
+
+$installer->startSetup();
+
+$prefix = Mage::getConfig()->getTablePrefix();
+
+$installer->run('
+    CREATE TABLE IF NOT EXISTS 
+    `' . $prefix . 'paymentmodule_recurrence_template` (
+        `id`                    int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        `is_disabled`           TINYINT NOT NULL DEFAULT 0,
+        `is_single`             TINYINT NOT NULL DEFAULT 0,
+        `name`                  VARCHAR(45) NULL,
+        `description`           TEXT NULL,
+        `accept_credit_card`    TINYINT NOT NULL DEFAULT 0,
+        `accept_boleto`         TINYINT NOT NULL DEFAULT 0,
+        `allow_installments`    TINYINT NOT NULL DEFAULT 0,
+        `due_type`              CHAR NOT NULL,
+        `due_value`             TINYINT NOT NULL DEFAULT 0,            
+        `trial`                 TINYINT NOT NULL DEFAULT 0,
+        `installments`          VARCHAR(45) NULL,
+        `created_at`            TIMESTAMP    DEFAULT current_timestamp, 
+        `updated_at`            TIMESTAMP    DEFAULT current_timestamp ON UPDATE current_timestamp
+    )
+'
+);
+
+$installer->endSetup();
