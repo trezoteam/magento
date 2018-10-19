c7ebad62397ab066395fbf3aee6240ec47d7c238
Michel Lima <michelpl@yahoo.com.br>
2018-10-19 18:45:14 -0300

    :card_file_box: Change timestamp collumn creation.

--- app/code/community/Mundipagg/Paymentmodule/sql/mundipagg_paymentmodule_setup/install-0.1.0.php
+++ app/code/community/Mundipagg/Paymentmodule/sql/mundipagg_paymentmodule_setup/install-0.1.0.php
@@ -7,20 +7,21 @@ $installer->startSetup();
 $prefix = Mage::getConfig()->getTablePrefix();
 
 $installer->run("
-CREATE TABLE paymentmodule_savedcreditcard
-(
-  id                    INT AUTO_INCREMENT PRIMARY KEY,
-  mundipagg_card_id     VARCHAR(255) NOT NULL,
-  holder_name           VARCHAR(255) NULL,
-  mundipagg_customer_id VARCHAR(255) NOT NULL,
-  customer_id           int(11)      NOT NULL,
-  brand_name            VARCHAR(12)  NOT NULL,
-  first_six_digits      VARCHAR(6)   NULL,
-  last_four_digits      VARCHAR(4)   NOT NULL,
-  expiration_date       DATE         NULL,
-  created_at            TIMESTAMP    DEFAULT now(), 
-  updated_at            TIMESTAMP    DEFAULT now() ON UPDATE now() 
-)
+  CREATE TABLE IF NOT EXISTS  
+    " . $prefix . "paymentmodule_savedcreditcard
+    (
+      id                    INT AUTO_INCREMENT PRIMARY KEY,
+      mundipagg_card_id     VARCHAR(255) NOT NULL,
+      holder_name           VARCHAR(255) NULL,
+      mundipagg_customer_id VARCHAR(255) NOT NULL,
+      customer_id           int(11)      NOT NULL,
+      brand_name            VARCHAR(12)  NOT NULL,
+      first_six_digits      VARCHAR(6)   NULL,
+      last_four_digits      VARCHAR(4)   NOT NULL,
+      expiration_date       DATE         NULL,
+      created_at            TIMESTAMP    DEFAULT current_timestamp, 
+      updated_at            TIMESTAMP    DEFAULT current_timestamp ON UPDATE current_timestamp 
+    )
 ;
 ");
 
