2d355153e693da82f919d401e180828e6f6e0a2d
Ian Lessa <ian.lessa1@gmail.com>
2019-05-22 20:29:32 -0300

    :recycle: using magento syntax to add a column

--- app/code/community/Mundipagg/Paymentmodule/sql/mundipagg_paymentmodule_setup/upgrade-2.1.0-2.1.2.php
+++ app/code/community/Mundipagg/Paymentmodule/sql/mundipagg_paymentmodule_setup/upgrade-2.1.0-2.1.2.php
@@ -2,13 +2,17 @@
 $installer = $this;
 
 $installer->startSetup();
-
 $prefix = Mage::getConfig()->getTablePrefix();
 
-$installer->run("
-          ALTER TABLE `" . $prefix . "paymentmodule_configuration`
-          ADD store_id VARCHAR(255);
-        "
+$table = $installer->getConnection()->addColumn(
+    $prefix . "paymentmodule_configuration",
+    'store_id',
+    [
+        'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+        'length' => 255,
+        'nullable' => true,
+        'comment' => 'store id'
+    ]
 );
 
 $installer->endSetup();
