976d053bee00be3a03e9876d816cd9843a3f3dc6
Ian Lessa <ian.lessa1@gmail.com>
2019-05-22 17:18:54 -0300

    :bug: fix jQuery redefinition and i18n::getTable encoding issue

--- app/code/community/Mundipagg/Paymentmodule/controllers/I18nController.php
+++ app/code/community/Mundipagg/Paymentmodule/controllers/I18nController.php
@@ -17,12 +17,27 @@ class Mundipagg_Paymentmodule_I18nController extends Mage_Core_Controller_Front_
             return $line[0] !== '#';
         });
 
-        $translateTable = (object) $translateTable;
+        //fix encoding errors;
+        $parsed = [];
+        array_walk($translateTable, function (&$line, $index) use (&$parsed) {
+
+            json_encode($line);
+            if (json_last_error() !== 0) {
+                $line = utf8_encode(mb_convert_encoding($line, 'UTF-8',"UTF-8"));
+            }
+            json_encode($index);
+            if (json_last_error() !== 0) {
+                $index = utf8_encode(mb_convert_encoding($index, 'UTF-8',"UTF-8"));
+            }
+            $parsed[$index] = $line;
+        });
+
+        $json = json_encode($parsed,JSON_FORCE_OBJECT);
 
         return $this->getResponse()
             ->clearHeaders()
             ->setHeader('HTTP/1.0', 200 , true)
             ->setHeader('Content-Type', 'text/html')
-            ->setBody(json_encode($translateTable));
+            ->setBody(json_encode($json));
     }
 }
